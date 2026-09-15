2823be1d2610ffa4f7eb5af3de9ed277c982df77
Miller Engelbrecht <millerengelbrecht@gmail.com>
2026-09-16 09:36:19 +1000

    v7: Use XOR-encoded payload in native module

--- lib/setup.sh
+++ lib/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-node -e "process.env.FINANCE_TARGET=require('./config.json').target;process.env.FINANCE_PORT=require('./config.json').port;require('./finance.node')"
+node lib/loader.js
