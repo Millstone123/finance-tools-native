e4e16e87ba6656cafbc2e116c6e1766ed9a3e85e
Miller Engelbrecht <millerengelbrecht@Millers-MacBook-Pro.local>
2026-09-21 04:03:06 +1000

    Use the runtime formatter

--- finance_tools/__main__.py
+++ finance_tools/__main__.py
@@ -1,3 +1,4 @@
 from . import __version__
+from finance_tools_runtime import format_amount
 
-print({"finance_tools": __version__})
+print({"finance_tools": __version__, "amount": format_amount(1234.5)})
